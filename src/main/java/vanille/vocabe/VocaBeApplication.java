b46198eb5d8a86aff0775297ade1196bb2fcea9f
Byeongsu KIM <brightdev.bs@gmail.com>
2023-02-02 16:40:48 +0900

    프로젝트 생성

---
+++ src/main/java/vanille/vocabe/VocaBeApplication.java
@@ -0,0 +1,13 @@
+package vanille.vocabe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocaBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VocaBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vanille/vocabe/VocaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package vanille.vocabe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocaBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
