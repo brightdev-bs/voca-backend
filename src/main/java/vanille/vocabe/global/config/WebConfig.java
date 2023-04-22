3ea9b4465337a15c06a86abcdc57eff109c62b3b
Byeongsu KIM <brightdev.bs@gmail.com>
2023-04-22 15:35:31 +0900

    voca-front/#8-variables  - docker compose 파일 수정  - webConfig 수정  - application.yml수정

--- src/main/java/vanille/vocabe/global/config/WebConfig.java
+++ src/main/java/vanille/vocabe/global/config/WebConfig.java
@@ -1,10 +1,8 @@
 package vanille.vocabe.global.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,14 +10,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Value("${front-server.host}")
+    @Value("${front-server}")
     private String FRONT_SERVER;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("FRONT_SERVER")
-                .allowedOriginPatterns("https://voca-world.com", "http://localhost")
+                .allowedOrigins(FRONT_SERVER)
+                .allowedOriginPatterns("https://voca-world.com")
                 .allowedMethods(
                         HttpMethod.GET.name(),
                         HttpMethod.HEAD.name(),
