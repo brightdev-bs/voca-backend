b51f5bb3edb379fa18ede10f69bc05f12580ab89
Byeongsu KIM <brightdev.bs@gmail.com>
2023-07-28 13:13:46 +0900

    feat/#23-스프링 부트 타임존 설정

--- src/main/java/vanille/vocabe/VocaBeApplication.java
+++ src/main/java/vanille/vocabe/VocaBeApplication.java
@@ -3,11 +3,19 @@ package vanille.vocabe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class VocaBeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(VocaBeApplication.class, args);
+    }
+
+    @PostConstruct
+    void started(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
 
 }
