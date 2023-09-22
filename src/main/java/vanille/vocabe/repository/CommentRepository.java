0a540c4c2f927fa638dfb30f9e9d98a66749ed1f
Byeongsu KIM <brightdev.bs@gmail.com>
2023-09-22 22:35:33 +0900

    커뮤니티 기능 제거

--- src/main/java/vanille/vocabe/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package vanille.vocabe.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import vanille.vocabe.entity.Comment;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
